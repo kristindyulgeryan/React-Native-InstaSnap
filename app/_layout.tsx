3c1944c5ab23306e4888062ada4691b00b8314c4
Kristin <kristindyulgeryan@abv.bg>
2025-05-22 16:37:04 +0300

    Create Convex schema with tables and indexes for users, posts, likes, comments, follows, notifications, and bookmarks

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,21 +1,15 @@
-import InitialLayout from "@/assets/images/components/InitialLayout";
-import { tokenCache } from "@/cache";
-import { ClerkLoaded, ClerkProvider } from "@clerk/clerk-expo";
-
-import { SafeAreaView } from "react-native";
-import { SafeAreaProvider } from "react-native-safe-area-context";
+import InitialLayout from "@/components/InitialLayout";
+import ClerkAndConvexProvider from "@/providers/ClerkAndConvexProvider";
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 
 export default function RootLayout() {
   return (
-    <ClerkProvider tokenCache={tokenCache}>
-      <ClerkLoaded>
-        <SafeAreaProvider>
-          <SafeAreaView style={{ flex: 1, backgroundColor: "black" }}>
-            <InitialLayout />
-            {/* <Stack screenOptions={{ headerShown: false }} /> */}
-          </SafeAreaView>
-        </SafeAreaProvider>
-      </ClerkLoaded>
-    </ClerkProvider>
+    <ClerkAndConvexProvider>
+      <SafeAreaProvider>
+        <SafeAreaView style={{ flex: 1, backgroundColor: "black" }}>
+          <InitialLayout />
+        </SafeAreaView>
+      </SafeAreaProvider>
+    </ClerkAndConvexProvider>
   );
 }
