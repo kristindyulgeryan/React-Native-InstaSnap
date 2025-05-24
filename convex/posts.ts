58ae03a51cb49ffda5af72254bdb776dab89bf77
Kristin <kristindyulgeryan@abv.bg>
2025-05-24 21:07:12 +0300

    Add post creation and image upload mutations with user post count update.

---
+++ convex/posts.ts
@@ -0,0 +1,47 @@
+import { v } from "convex/values";
+import { mutation } from "./_generated/server";
+
+export const generateUploadUrl = mutation(async (ctx) => {
+  const identitiy = await ctx.auth.getUserIdentity();
+  if (!identitiy) throw new Error("Unauthorized");
+  return await ctx.storage.generateUploadUrl();
+});
+
+export const createPost = mutation({
+  args: {
+    caption: v.optional(v.string()),
+    storageId: v.id("_storage"),
+  },
+  handler: async (ctx, args) => {
+    const identitiy = await ctx.auth.getUserIdentity();
+    if (!identitiy) throw new Error("Unauthorized");
+
+    const currentUser = await ctx.db
+      .query("users")
+      .withIndex("by_clerk_id", (q) => q.eq("clerkId", identitiy.subject))
+      .first();
+
+    if (!currentUser) throw new Error("User not found");
+
+    const imageUrl = await ctx.storage.getUrl(args.storageId);
+    if (!imageUrl) throw new Error("Image URL not found");
+
+    // create post
+    const postId = await ctx.db.insert("posts", {
+      user: currentUser._id,
+      imageUrl,
+      storageId: args.storageId,
+      caption: args.caption,
+      likes: 0,
+      comments: 0,
+    });
+
+    // increment user's post by 1
+
+    await ctx.db.patch(currentUser._id, {
+      posts: currentUser.posts + 1,
+    });
+
+    return postId;
+  },
+});
